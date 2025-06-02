04652e65eb08f06b53bb999de1795d56a3719fc9
Madbob1010 <technikmain998@gmail.com>
2025-06-01 22:46:32 -0300

    V.2 on mamba env and modularized backtest file and config editor initalization.

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch, talib, vectorbt, plotly, dash
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
-print(talib.__version__)
-print(vectorbt.__version__)
-print(plotly.__version__)
-print(dash.__version__)
